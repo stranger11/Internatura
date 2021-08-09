5c7b80ece5bdc3543b47aa082e53acbba74edcf5
Yan <yanislav888@mail.ru>
2021-08-09 16:12:04 +0300

    task 15-1

--- app/src/main/java/com/example/internatura/MainActivity.kt
+++ app/src/main/java/com/example/internatura/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import kotlinx.coroutines.*
 import timber.log.Timber
+import java.io.BufferedReader
+import java.io.InputStreamReader
 import java.net.URL
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,39 +19,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-
         Thread {
             val response = URL("https://api.flickr.com/services/rest/?method=flickr.photos" +
                     ".search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format" +
                     "=json&nojsoncallback=1").readText()
-            Timber.d(response)
+            val json = Gson()
+
+            val d = json.fromJson(response, CommentResponse::class.java)
+
+            Timber.d(d.toString())
         }.start()
 
-
-       // GlobalScope.launch(Dispatchers.IO) {
-       //     val response = URL("https://api.flickr.com/services/rest/?method=flickr.photos" +
-       //             ".search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format" +
-       //             "=json&nojsoncallback=1").readText()
-       //     Timber.d(response)
-
-          //  val products = Gson().fromJson(response, Array<Photo>::class.java).toList()
-
-      // }
-
-
-
-
-
-
     }
-
-
-
-
-
-
-
-
 
 }
 
