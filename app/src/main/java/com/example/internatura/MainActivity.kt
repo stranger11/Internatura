826bbaf2ffe866575e757ea05026a5b177018a49
Yan <yanislav888@mail.ru>
2021-08-09 16:20:59 +0300

    task 15-1 поправил замечания

--- app/src/main/java/com/example/internatura/MainActivity.kt
+++ app/src/main/java/com/example/internatura/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.internatura
 
 import android.os.Bundle
+import android.provider.SyncStateContract
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import kotlinx.coroutines.*
@@ -10,28 +11,23 @@ import java.io.InputStreamReader
 import java.net.URL
 
 
+private const val URL_STRING = "https://api.flickr.com/services/rest/?method=flickr.photos" +
+        ".search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format" +
+        "=json&nojsoncallback=1"
+
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
         Thread {
-            val response = URL("https://api.flickr.com/services/rest/?method=flickr.photos" +
-                    ".search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format" +
-                    "=json&nojsoncallback=1").readText()
-            val json = Gson()
-
-            val d = json.fromJson(response, CommentResponse::class.java)
-
-            Timber.d(d.toString())
+            val response = URL(URL_STRING).readText()
+            val gson = Gson()
+            val flickResponse = gson.fromJson(response, CommentResponse::class.java)
+            Timber.d(flickResponse.toString())
         }.start()
-
     }
-
 }
 
 
