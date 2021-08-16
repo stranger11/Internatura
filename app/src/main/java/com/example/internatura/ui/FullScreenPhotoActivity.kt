025c9791c91da61f8a3c06126de08cd5e562b45d
Yan <yanislav888@mail.ru>
2021-08-16 14:13:19 +0300

    17 task

--- app/src/main/java/com/example/internatura/ui/FullScreenPhotoActivity.kt
+++ app/src/main/java/com/example/internatura/ui/FullScreenPhotoActivity.kt
@@ -2,7 +2,11 @@ package com.example.internatura.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.example.internatura.R
 import com.example.internatura.databinding.ActivityFullScreenPhotoBinding
 import com.example.internatura.util.EXTRA_LINK_PHOTO
 
@@ -20,5 +24,21 @@ class FullScreenPhotoActivity : AppCompatActivity() {
     private fun displayPhoto() {
         val linkFromFirstActivity = intent.getStringExtra(EXTRA_LINK_PHOTO)
         Glide.with(this).load(linkFromFirstActivity).into(mBinding.imagePhoto)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_favorites -> {
+                onDestroy()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
